8c2dbf7eac925846c8d261bca2bbe222eb33b5e1
Tomas Hornak <hornak@takeplace.eu>
2018-03-27 23:13:36 +0200

    auto end-turn checkbox added

--- kdice-mobile-bookmarklet.js
+++ kdice-mobile-bookmarklet.js
@@ -1,4 +1,5 @@
 javascript:(function () {
+    /* Adds CSS script and viewport meta tags into html head */
     const s = document.createElement('link');
     s.setAttribute('href', 'https://rawgit.com/tomas223/kdice-mobile/master/styles/kdice-mobile-style.css');
     s.setAttribute('rel', 'stylesheet');
@@ -11,5 +12,31 @@ javascript:(function () {
     document.getElementsByTagName('head')[0].appendChild(m);
     document.getElementsByTagName('head')[0].appendChild(s);
 
-    console.log('Custom CSS stylesheet injected');
+    /* Adds auto end-turn checkbox */
+    const btn = document.querySelector('.iogc-Controls button');
+
+    const lbl = document.createElement('label');
+    const txt = document.createTextNode('Auto End-Turn');
+    const chckbx = document.createElement('input');
+    chckbx.setAttribute('type', 'checkbox');
+    lbl.appendChild(chckbx);
+    lbl.appendChild(txt);
+
+    document.querySelector('.iogc-Controls').appendChild(lbl);
+
+    const btnObserver = new MutationObserver((mutationsList) => {
+        for (let mutation of mutationsList) {
+            if (
+                mutation.attributeName === 'aria-hidden'
+                && btn.getAttribute('aria-hidden') === null
+                && chckbx.checked
+            ) {
+                setTimeout(() => btn.click(), 100);
+            }
+        }
+    });
+
+    btnObserver.observe(btn, { attributes: true });
+
+    console.log('custom Kdice mobile script injected');
 })();
